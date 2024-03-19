0afaa05d55badec9f043402f36eed78f64fa5817
Tanish <tanishpashte.24@gmail.com>
2024-03-19 22:28:13 +0530

    lecture 2 covered

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 23119048
+let accountEmail = "fakemail@gmail.com"
+let accountPassword = "39241803"
+let accountCity;
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //treats all JS code as new version
+
+//data types : number, bigint, string, boolean, null, undefined, symbol, objects
+
+console.log(typeof null);
